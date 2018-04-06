8ae8c9078e002903df3737470a85afb6c066271a
Charlie <heinercj@gmail.com>
2018-04-06 08:57:18 -0700

    added in delete and edit icon. not set up yet

--- src/components/round/Round.js
+++ src/components/round/Round.js
@@ -3,6 +3,10 @@ import './round.css';
 
 export default class Round extends Component{
 
+  handleDelete(id){
+
+  }
+
   render(){
     const { roundStats } = this.props;
     const holes = Array(18).fill('');
@@ -11,6 +15,10 @@ export default class Round extends Component{
         <div>
           <h1>{roundStats.course}</h1>
           <p id="date">{roundStats.date}</p>
+          <div id='editButtons'>
+            <div>✎ round</div>
+            <div onClick={this.handleDelete(roundStats.key)}>🗑 round</div>
+          </div>
         </div>
         <div id="item1">
           <h5>Score:{roundStats.totalScore}</h5>
@@ -21,8 +29,8 @@ export default class Round extends Component{
         <section id="grid">
           <div className="title">Hole</div>{holes.map((h, i) => <div className="title" key={i} id={i}>{i + 1}</div>)}
           <div className="title">Score</div>{roundStats.holesScore.map((s, i) => <div className="title" key={i} id={i}>{s}</div>)}
-          <div className="title">FIR</div>{roundStats.fir.map((f, i) => <div className="title" key={i} id={i}>{f}</div>)}
-          <div className="title">GIR</div>{roundStats.gir.map((g, i) => <div className="title" key={i} id={i}>{g}</div>)}
+          <div className="title">FIR</div>{roundStats.fir.map((f, i) => <div className="title" key={i} id={i}>{(f) ? '✓' : ''}</div>)}
+          <div className="title">GIR</div>{roundStats.gir.map((g, i) => <div className="title" key={i} id={i}>{(g) ? '✓' : ''}</div>)}
           <div className="title">Putts</div>{roundStats.putts.map((p, i) => <div className="title" key={i} id={i}>{p}</div>)}
         </section>
       </div>
