b5faba6ad7ca2f3efbdcd66f0efbc617441c9bff
Charlie <heinercj@gmail.com>
2019-07-01 09:59:17 -0700

    not sure

--- src/components/editForm/actions.js
+++ src/components/editForm/actions.js
@@ -6,7 +6,7 @@ const players = db.ref('players');
 export function getRound(name, id){
   return {
     type: GET_ROUND,
-    payload: {name: id}
+    payload: players.child(id).once('value').then(rd => { return rd; })
 
   };
 }
