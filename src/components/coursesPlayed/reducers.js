3788fd63d155e0b4da17d5539780fc6aa0860b6d
Charlie <heinercj@gmail.com>
2020-02-27 13:13:18 -0800

    alphabatize courses played list

--- src/components/coursesPlayed/reducers.js
+++ src/components/coursesPlayed/reducers.js
@@ -1,6 +1,6 @@
 export const LOAD_COURSES_PLAYED = 'LOAD_COURSES_PLAYED';
 
-export function coursesPlayed(state = {}, { type, payload }){
+export function coursesPlayed(state = [], { type, payload }){
   switch(type){
     case LOAD_COURSES_PLAYED:
       return payload;
