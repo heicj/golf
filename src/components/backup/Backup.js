4a9f2755e76cdbfe19d8692bf881254f91a1d017
Charlie <heinercj@gmail.com>
2020-03-27 09:26:52 -0700

    backup lists styled

--- src/components/backup/Backup.js
+++ src/components/backup/Backup.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { backupDates, backup, getDownloadData } from './backupFuncs';
 import { connect } from 'react-redux';
+import './backup.css';
 
 class Backup extends Component{
   
@@ -16,14 +17,29 @@ class Backup extends Component{
   render(){
     const { dates, downloads } = this.props;
     return (
-      <section>
-        <button onClick={this.handleBackup}>Backup</button>
-        <ul>
-          {dates.map((d, i) => <li key={i}>{d}</li>)}
-        </ul>
-        <ul>
-          {downloads.map((d, i) => <li key={i}>date: {d[0]}  time: {d[1]}</li>)}
-        </ul>
+      <section id='mainSection'>
+        {
+          dates ? 
+            <section className='dataSection'>
+              <h2 className='titles'>LAST 10 BACKUP DATES</h2>
+       
+              <button id='backupButton' onClick={this.handleBackup}>Backup</button>
+              <ol>
+                {dates.slice(0, 10).map((d, i) => <li key={i}>{d}</li>)}
+              </ol>
+            </section>
+            : null  
+        }
+        {
+          downloads ? 
+            <section className='dataSection'>
+              <h2 className='titles'>LAST 10 DOWNLOADS</h2>
+              <ol>
+                {downloads.reverse().slice(0, 10).map((d, i) => <li key={i}>date: {d[0]}  time: {d[1]}</li>)}
+              </ol>
+            </section>
+            : null
+        }
       </section>
     );
   }
