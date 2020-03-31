bc33de74ac2050c76cc843381a7e15a09aa492c6
Charlie <heinercj@gmail.com>
2020-03-31 12:58:27 -0700

    func to convert rd or rds to chart format done and tested

--- src/components/chart/DataChart.js
+++ src/components/chart/DataChart.js
@@ -9,14 +9,46 @@ export default class DataChart extends Component{
   }
 
   state = {
+    // data: {
+    //   labels: ["January", "February", "March", "April", "May", "June", "July"],
+    //   datasets: [
+    //     {
+    //       label: 'second dataset',
+    //       backgroundColor: 'rgb(50, 50, 50)',
+    //       borderColor: 'rgb(10,10,10)',
+    //       data: [10, 2, 30, 4, 5, 6, 20]
+    //     },
+    //     {
+    //     label: "My First dataset",
+    //     backgroundColor: 'rgb(255, 99, 132)',
+    //     borderColor: 'rgb(255, 99, 132)',
+    //     data: [0, 10, 5, 2, 20, 30, 45],
+    //   }]
+    // }
     data: {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
-      datasets: [{
-        label: "My First dataset",
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
-      }]
+      labels: ['3-31-2020 chehalem glenn', '7-2-2020 old macdonald'],
+      datasets: [
+        { label: 'FIR',
+          backgroundColor:  'rgba(10, 20, 30, 20)',
+          borderColor: 'rgba(10, 20, 30, 20)',
+          data: [10, 8]
+        },
+        { label: 'GIR',
+          backgroundColor: 'rgba(15, 25, 35, 25)', 
+          borderColor: 'rgba(15, 25, 35, 25)', 
+          data: [1, 2]
+        },
+        { label: 'Putts',
+          backgroundColor: 'rgba(20, 25, 40, 30)', 
+          borderColor: 'rgba(20, 25, 40, 30)', 
+          data: [36, 38]
+        },
+        { label: 'Score',
+          backgroundColor: 'rgba(50, 20, 60, 20)',
+          borderColor: 'rgba(50, 20, 60, 20)',
+          data: [72, 71]
+        }
+      ]
     }
   }
   render(){
---
+++ src/components/chart/actions.js
@@ -0,0 +1,102 @@
+/*
+data structure will be
+
+data: {
+  labels: []
+  datasets: [
+    { label: 'FIR'
+      backgroundColor: 
+      borderColor:
+      data: []
+    },
+    { label: 'GIR'
+      backgroundColor: 
+      borderColor:
+      data: []
+    },
+    { label: 'Putts'
+      backgroundColor: 
+      borderColor:
+      data: []
+    },
+    { label: 'Score'
+      backgroundColor: 
+      borderColor:
+      data: []
+    }
+  ]
+}
+get rounds for player.
+
+*/
+
+//make a function that takes an arr
+//turns it into format that can be given to graph component.
+//if array is one item aka just a round then it fills data arr
+//with each hole info. if arr given to it contains many rounds
+//then fills array with totals of each round.
+
+export function createGraphData(arr){
+  let sortedData = {};
+  let singleRound;
+  let labels = [];
+  let fir = [];
+  let gir = [];
+  let putts = [];
+  let score = [];
+
+  arr.length == 1 ? singleRound = true : false;
+  singleRound ? labels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18] : [];
+
+  if(singleRound){
+    //single round graph data graphs hole by hole data
+    let round = arr[0];
+    putts = round.putts;
+    score = round.holeScore;
+    //changes true/false values into 1 or 0 and adds to arrays
+    for(let i = 0; i < round.fir.length; i++){
+      let holeFir = round.fir[i] ? 1 : 0;
+      let holeGir = round.gir[i] ? 1 : 0;
+      fir.push(holeFir);
+      gir.push(holeGir);
+    }
+  } else {
+    //if setting graph data for multiple rounds then totals for each round are used
+    for(let j = 0; j < arr.length; j++){
+      let r = arr[j];
+      let date = r.date.length > 1 ? r.date : 'NA';
+      labels.push(date + ' ' + r.course);
+      fir.push(r.totalFir);
+      gir.push(r.totalGir);
+      putts.push(r.totalPutts);
+      score.push(r.totalScore);
+    }
+  }
+
+  sortedData.labels = labels;
+  sortedData.datasets = [
+    { 'label': 'FIR',
+      'backgroundColor': 'rgba(10, 20, 30, 20)',
+      'borderColor': 'rgba(10, 20, 30, 20)',
+      'data': fir
+    },
+    { 'label': 'GIR',
+      'backgroundColor': 'rgba(15, 25, 35, 25)', 
+      'borderColor': 'rgba(15, 25, 35, 25)',
+      'data': gir
+    },
+    { 'label': 'Putts',
+      'backgroundColor': 'rgba(20, 25, 40, 30)', 
+      'borderColor': 'rgba(20, 25, 40, 30)', 
+      'data': putts
+    },
+    { 'label': 'Score',
+      'backgroundColor': 'rgba(50, 20, 60, 20)', 
+      'borderColor': 'rgba(50, 20, 60, 20)',
+      'data': score
+    }
+  ];
+  
+  return sortedData;
+  
+}
