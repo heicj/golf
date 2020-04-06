4680559e052b81fa2a393d198bc3b78c0c335e3b
Charlie <heinercj@gmail.com>
2020-04-06 13:44:07 -0700

    course avg view displays times played each course

--- src/components/courseAvgRdView/CourseAvgRdView.js
+++ src/components/courseAvgRdView/CourseAvgRdView.js
@@ -6,6 +6,7 @@ export default class CourseAvgRdView extends Component{
     return (
       <section>
         <h2>{course}</h2>
+        <h3>Times played: {rd.score.length}</h3>
         {Object.keys(rd).map((stat, i) => {
           let total = rd[stat].reduce((a, c) => a + c);
           let avg = (total / rd[stat].length).toFixed(2);
